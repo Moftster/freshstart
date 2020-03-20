e1d11350b1996467fad66bd34dccb1d27736474a
David Moftakhar <david@sellmysnaps.com>
2020-03-20 10:31:15 +0000

    Database seeder with user information.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Derek Williams',
+            'email' => 'derek@freshstartpsychotherapy.co.uk',
+            'password' => bcrypt('password'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Annie Foley',
+            'email' => 'annie@freshstartpsychotherapy.co.uk',
+            'password' => bcrypt('password'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Christa Scholtz',
+            'email' => 'christa@freshstartpsychotherapy.co.uk',
+            'password' => bcrypt('password'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Spencer Clifford',
+            'email' => 'spencer.clifford@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
