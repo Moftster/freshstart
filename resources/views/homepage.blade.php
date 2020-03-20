465cc0f29db125b60130bdebae1ca8294dfd48f2
David Moftakhar <david@sellmysnaps.com>
2020-03-20 16:41:45 +0000

    Removed previous homepage and login welcome message.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,15 +2,6 @@
 
 @section('content')
 <div class="container">
-    
-        <div class="alert alert-success text-center" role="alert">
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
-            You are logged in!
-        </div>
 
         <div class="row justify-content-center">
             <div class="col-md-12">
