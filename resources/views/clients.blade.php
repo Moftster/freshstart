fe4c1cf812bd5c847ffdab48cd571070c03fa453
David Moftakhar <david@sellmysnaps.com>
2020-03-21 15:34:20 +0000

    Created blank clients page.

---
+++ resources/views/clients.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <h1>Clients</h1>
+                <form>
+                    <div class="row">
+                      <div class="col">
+                        <input type="text" class="form-control" placeholder="First name">
+                      </div>
+                      <div class="col">
+                        <input type="text" class="form-control" placeholder="Last name">
+                      </div>
+                    </div>
+                  </form>
+                
+            </div>
+        </div>
+
+</div>
+
+
+@endsection
