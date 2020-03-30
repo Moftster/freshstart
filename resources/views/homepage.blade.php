61664e94c43a1e5026b0814b4e2f4cb4b7931a13
David Moftakhar <david@sellmysnaps.com>
2020-03-30 12:04:18 +0100

    Text color changed.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -28,8 +28,8 @@
 
       <div class="showcase-content">
           <div class="p-5">
-              <h1>FreshStart Psychotherapy</h1>
-              <p class="my-1">
+              <h1 class="hero-text">FreshStart Psychotherapy</h1>
+              <p class="my-1 hero-text">
                 Low cost and subsidised counselling and psychotherapy.
               </p>
               <a href="#" class="btn-primary">Learn More</a>
