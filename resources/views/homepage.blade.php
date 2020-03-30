4600d0c12b6eac5a0fe089ed31644ccfd987125a
David Moftakhar <david@sellmysnaps.com>
2020-03-30 12:12:39 +0100

    Removed text shadow.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,30 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <div class="container">
-    <div class="jumbotron banner-text">
-        <h1 class="display-4"></h1>
-        <p class="lead">Low cost and subsidised counselling and psychotherapy.</p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p class="lead">
-          <a class="btn btn-primary btn-lg" href="#" role="button">Find out more</a>
-        </p>
-      </div>
-
-</div> --}}
 
 
-{{-- <header class="showcase"> --}}
   <div class="container">
-      {{-- <nav>
-          <h1 class="logo">FreshStart Psychotherapy</h1>
-          <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Services</a></li>
-          </ul>
-      </nav> --}}
 
       <div class="showcase-content">
           <div class="p-5">
@@ -39,17 +18,7 @@
 
       </div>
       
-      {{-- <header class="hero">
-        <div class="center-content">
-          <h1>An Article Title</h1>
-          <h3>A longer subtitle but still important</h3>
-          <a href="#" class="button">Some Action Here</a>
-        </div>
-      </header> --}}
-
   </div>
-{{-- </header> --}}
-
 
 
 @endsection
