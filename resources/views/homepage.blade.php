ef5192512bc95b099fb362bdca1be3084d913202
David Moftakhar <david@sellmysnaps.com>
2020-03-31 10:13:04 +0100

    Banner works on big screen and imported tree of life logo.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,15 +7,28 @@
 
       <div class="showcase-content">
           <div class="p-5">
+          <div>
             <img src="{{ asset('img/logo.jpg') }}" alt="">
+          </div>
+          <div class="py-1">
               <a href="#" class="btn-primary">Learn More</a>
               <a href="#" class="btn-secondary">Sign Up</a>
           </div>
-
-          <img src="{{ asset('img/logo.jpg') }}" alt="">
-
+          </div>
 
       </div>
+
+      <div class="">
+        <div class="p-5">
+        <div>
+          <img src="{{ asset('img/treeoflife.jpg') }}" alt="">
+        </div>
+        <div class="py-1">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita officiis eligendi, magni eius possimus non eaque deleniti quia? Perspiciatis accusantium ex sunt eveniet, ea error blanditiis quisquam obcaecati. Quaerat cum tenetur accusantium eum dolor inventore provident nostrum accusamus distinctio magnam, dolorem minima aperiam quas eaque, ad error repudiandae aliquid odio perferendis aspernatur necessitatibus itaque. Consectetur architecto ratione sit quae libero qui ipsum placeat asperiores, quaerat labore! Ad voluptates alias inventore error dolorum vero debitis modi, eveniet placeat numquam sed, ipsum dolorem incidunt delectus odit nesciunt nulla! Ipsam eius neque sequi, maxime nesciunt ad sit est, maiores hic perferendis, at perspiciatis?</p>
+        </div>
+        </div>
+
+    </div>
       
   </div>
 
