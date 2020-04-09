a6327ee0056cebad50ff86760e49da5bebd7a5e1
David Moftakhar <david@sellmysnaps.com>
2020-04-09 10:39:47 +0100

    Our people page formatted for responsive.

--- resources/views/ourpeople.blade.php
+++ resources/views/ourpeople.blade.php
@@ -10,7 +10,7 @@
 
             <h3>Life Journey</h3>
             
-            <p>My background is in the Arts and I worked as an art director/designer in theatre, film and television before training as a therapist. I have worked in South Africa, Canada, USA, France and the UK, where I now reside. I set up FreshStart in 1999 with 3 other colleagues, as I could see that there was a need for affordable long-term therapy and the need for therapist to work with clients. As I supervise many of the trainee therapist,  I know just how dedicated they are to serve their clients to the best of their abilities.  I now practice as a UKCP accredited and registered Psychotherapist and supervisor. I adhere to the rules of professional conduct for UKCP.</p>
+            <p>My background is in the Arts and I worked as an art director/designer in theatre, film and television before training as a therapist. I have worked in South Africa, Canada, USA, France and the UK, where I now reside. I set up FreshStart in 1999 with 3 other colleagues, as I could see that there was a need for affordable long-term therapy and the need for therapist to work with clients. As I supervise many of the trainee therapist,  I know just how dedicated they are to serve their clients to the best of their abilities.  I now practice as a <strong> UKCP accredited and registered</strong> Psychotherapist and supervisor. I adhere to the rules of professional conduct for UKCP.</p>
             
             <h3>Philosophy</h3>
             
@@ -22,7 +22,7 @@
             
             <h3>My experience</h3>
             
-            <p>In addition to my work as a therapist and supervisor I run many workshops on various subjects such as listening skills, PND, attachment theory and many other topics.  I also work with another Charity called Home-Start Westminster where we support families with at least one child under the age of 5. I have set up various projects within this charity i.e. Bump-Start that supports pregnant women and their babies until the baby is 1 years old.</p>
+            <p>In addition to my work as a therapist and supervisor I run many workshops on various subjects such as listening skills, PND, attachment theory and many other topics.  I also work with another Charity called <a href="https://www.homestartwestminster.org.uk/">Home-Start Westminster</a> where we support families with at least one child under the age of 5. I have set up various projects within this charity i.e. Bump-Start that supports pregnant women and their babies until the baby is 1 years old.</p>
             </div>
             <div class="content content-img">
             <img class="img-itself" src="{{ asset('img/derekwilliams.jpg') }}" alt="">
@@ -32,19 +32,19 @@
 
             <h3>Qualifications</h3>
             
-            <p>I hold a diploma in Transpersonal  and Integrative Psychotherapy from the Centre of Counselling and Psychotherapy  Education (CCPE); a Diploma  in Clinical Hypnotherapy from the London College of Clinical Hypnosis (LCCH),  have a degree in law (LLB) and have worked as a Solicitor in the City of London  for many years. I adhere to the rules of professional conduct for UKCP, BACP and  BSCH.</p>
+            <p>I hold a diploma in Transpersonal and Integrative Psychotherapy from the <strong>Centre of Counselling and Psychotherapy Education (CCPE)</strong>; a <strong>Diploma in Clinical Hypnotherapy</strong> from the London College of Clinical Hypnosis (LCCH),  have a degree in law (LLB) and have worked as a Solicitor in the City of London  for many years. I adhere to the rules of professional conduct for <strong>UKCP</strong>, <strong>BACP</strong> and  <strong>BSCH</strong>.</p>
              
             <h3>Professional Bodies</h3>
             
-            <p>I am a member of the British Association of Counselling and Psychotherapy (MBACP) and I am a registered and accredited Psychotherapist with the United Kingdom Council for Psychotherapy  (UKCP).  I am also afull member of the British Society of Clinical Hypnosis (BSCH).</p>
+            <p>I am a member of the <strong>B ritish Association of Counselling and Psychotherapy (MBACP)</strong> and I am a registered and accredited Psychotherapist with the United Kingdom Council for Psychotherapy  (UKCP).  I am also a full member of the <strong>British Society of Clinical Hypnosis (BSCH)</strong>.</p>
              
             <h3>Training</h3>
             
-            <p>I spent three years successfully completing a course in hypnotherapy I then completed a further further five years training with CCPE for a post graduate diploma in Integrative Psychotherapy. I undertake regular CPD courses. Previously I had a successful career in the City working in the legal and financial sectors which has made me very aware of the stresses and strains of working in high pressured environments.</p>
+            <p>I spent three years successfully completing a course in hypnotherapy I then completed a further further five years training with <strong>CCPE</strong> for a post graduate diploma in Integrative Psychotherapy. I undertake regular CPD courses. Previously I had a successful career in the City working in the legal and financial sectors which has made me very aware of the stresses and strains of working in high pressured environments.</p>
              
             <h3>My Work</h3>
             
-            <p>In addition to my private practice as a psychotherapist I also work with the NHS in a secure mental health unit in south London. During the past I have worked as a volunteer with various mental health and sexual health organisations providing both long term and short term psychotherapy and counselling. In addition I am a trustee and director of a small charity called Freshstart Psychotherapy. Prior to completing my training with CCPE I worked as a lawyer and was a partner of and departmental head to a firm of Solicitors in the City of London.</p>
+            <p>In addition to my private practice as a psychotherapist I also work with the NHS in a secure mental health unit in south London. During the past I have worked as a volunteer with various mental health and sexual health organisations providing both long term and short term psychotherapy and counselling. In addition I am a trustee and director of a small charity called Freshstart Psychotherapy. Prior to completing my training with <strong>CCPE</strong> I worked as a lawyer and was a partner of and departmental head to a firm of Solicitors in the City of London.</p>
             
             <h3>My Approach</h3>
             
