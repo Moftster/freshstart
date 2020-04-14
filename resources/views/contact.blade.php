9688b81d1d18c8b119be500aaf9a34ca621e7835
David Moftakhar <david@sellmysnaps.com>
2020-04-14 10:24:00 +0100

    Contact form complete.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,7 +9,7 @@
           <div class="company-info">
             <h3>FreshStartPsychotherapy</h3>
             <ul>
-              <li><i class="fa fa-home"></i>2 Warwick Cres, Little Venice, London W2 6NE</li>
+              <li><i class="fa fa-home"></i>2 Warwick Crescent, Little Venice, London W2 6NE</li>
               <li><i class="fa fa-phone"></i>020 7691 7638</li>
               <li class="hidden-micro"><i class="fa fa-envelope"></i><a href="mailto:enquiries@freshstartpsychotherapy.co.uk">enquiries@freshstartpsychotherapy.co.uk</a></li>
             </ul>
