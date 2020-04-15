ada84f7c044a2ac17e0cd909e2f8a1bea97f65c0
David Moftakhar <david@sellmysnaps.com>
2020-04-15 11:15:44 +0100

    User menus added on login.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,12 +91,18 @@
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                        <li class="item">
+                            <a href="#" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Add content
                             </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        </li>
+                        <li class="item">
+                            <a href="#" aria-haspopup="true" aria-expanded="false" v-pre>
+                                User Management
+                            </a>
+                        </li>
+                        
+                        <li class="item">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
@@ -106,7 +112,6 @@
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
-                            </div>
                         </li>
                     @endguest
                 </ul>
