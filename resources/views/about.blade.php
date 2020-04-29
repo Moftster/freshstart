6ed9481f584589976e148bc1df88a4ec57e24aae
David Moftakhar <david@sellmysnaps.com>
2020-04-29 12:42:02 +0100

    About page text and images completed.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -32,25 +32,18 @@ The FS structure ensures that the therapist receives expert supervision so that 
         <h2 class=""><i>"Our wounds are often the openings to the best and most beautiful part of us" - David Richo</i></h2>
     </div>
     <div class="about-text-2">
-        <p>When a client makes contact with the service they are invited to come for an assessment session with a UKCP registered psychotherapist.  Based on an individual's needs and availability, they will then be carefully matched and referred to a therapist who seems the most suitable for working with that particular client.</p>
-        <p><strong>Unwaged clients and students pay £12.00 per session</strong> and <strong>earning clients pay £12.00-£25.00 per session</strong> depending on what they can afford.</p>
-        <p><strong>For a limited number of clients in exceptional need we may be able to offer fees of £5 per session</strong>. We may have a waiting list for low fee paying clients. <strong>When clients are earning incomes above £25,000, we refer them on to ex-FreshStart volunteers who have completed their training and who are willing/able to take comparatively low-cost private referrals.  In such cases, clients generally pay fees of between £30-£60</strong>.</p>
-    </div>
-
+        <h3>Our History</h3>
+        <p>FS, in its original incarnation, started off as a very small set-up in 1999, when three psychotherapist at CCPE (Centre for Counselling and Psychotherapy Education), recognised that there was a need for affordable short and long term counselling and psychotherapy and for trainees to have a well run placement that would prepare them to set up their own practices once they were qualified.  For some years it operated under the umbrella of the CCPE, but in January 2009, having grown considerably and established itself as a service within the community, FreshStart Psychotherapy formed a limited company, and in September 2009 it was awarded charitable status. In 2016 the company changed it's status to form a Community Interest Company (C.I.C.) and future plans include expansion to other areas of London.</p>
+        </div>
+    
     <div class="quote-text">
+
         <h2 class=""><i>"Think of your head as an unsafe neighbourhood, don't go there alone." - David Richo</i></h2>
     </div>
 
     <div class="about-image-2">
         <img class="img-itself" src="{{ asset('img/people.jpg') }}" alt="">
     </div>
-
-    <div class="about-text-2">
-        <p>When a client makes contact with the service they are invited to come for an assessment session with a UKCP registered psychotherapist.  Based on an individual's needs and availability, they will then be carefully matched and referred to a therapist who seems the most suitable for working with that particular client.</p>
-        <p><strong>Unwaged clients and students pay £12.00 per session</strong> and <strong>earning clients pay £12.00-£25.00 per session</strong> depending on what they can afford.</p>
-        <p><strong>For a limited number of clients in exceptional need we may be able to offer fees of £5 per session</strong>. We may have a waiting list for low fee paying clients. <strong>When clients are earning incomes above £25,000, we refer them on to ex-FreshStart volunteers who have completed their training and who are willing/able to take comparatively low-cost private referrals.  In such cases, clients generally pay fees of between £30-£60</strong>.</p>
-    </div>
-
 
 </div>
 </div>
